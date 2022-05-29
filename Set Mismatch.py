6666273347453c9133fbee0bf26317a71f92021b
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-28 21:30:40 -0700

    Add files via upload

--- Set Mismatch.py
+++ Set Mismatch.py
@@ -17,3 +17,13 @@ class Solution:
             if lookup[i] > 1:
                 rep = i
         return [rep,mis]
+
+        # Solution 2
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums) + 1)) - sum(set(nums))]
+
+        # Solution 3
+        n = len(nums)    
+        s = n*(n+1)//2   # This code will calculate sum of 1-n numbers. please refer to Arithmetic Progression from Maths
+        miss = s - sum(set(nums))  # set(num) will return unique elements from the list and the sum(set(nums)) will calculate the sum of unique elements from the list.
+        duplicate = sum(nums) + miss - s # sum(nums) + miss no - sum of 1-n numbers will give us the duplicate
+        return [duplicate, miss]
